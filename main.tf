d4af9a88d6daeb6907782e7f984c638c3a1ec109
Avinash R <avinash.ravipati2@gmail.com>
2022-11-27 12:46:29 -0600

    Changes

--- main.tf
+++ main.tf
@@ -1,11 +1,50 @@
  provider "aws" {
     region = "us-east-1"
-    shared_config_files      = ["./config/conf"]
  }
 
 resource "aws_cloudwatch_log_group" "Glue_logs" {
    name = "Glue_logs"
    retention_in_days = 3
+}
+
+resource "aws_iam_role" "terraform_glue_role" {
+  name = "terraform_glue_role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal: {
+           Service: "glue.amazonaws.com"
+            },
+        Action: "sts:AssumeRole"
+      },
+    ]
+  })
+
+}
+
+resource "aws_iam_role_policy" "terraform_glue_role_policy" {
+  name = "terraform_glue_role_policy"
+  role = aws_iam_role.terraform_glue_role.id
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "S3:*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
 }
 
 resource "aws_s3_bucket" "glue" {
@@ -16,22 +55,10 @@ resource "aws_glue_job" "test_py_job" {
    name = "test_py_job"
    max_capacity = 0.0625
    #max_retries = 2
-   role_arn = "arn:aws:iam::751271771288:role/terraform_glue_role"
-
+   role_arn = aws_iam_role.terraform_glue_role.arn
    command {
      name = "pythonshell"
      script_location = "S3://${aws_s3_bucket.glue.bucket}/hello_world.py"
      python_version = 3
-
    }
-
 }
-
-#resource "aws_glue_trigger" "test_py_job_trigger" {
-#   name = "test_py_job_trigger"
-#   schedule = "cron(20 3 20 11 * 2022)"
-#   type = "SCHEDULED"
-#   actions {
-#     job_name=test_py_job
-#   }
-#}
